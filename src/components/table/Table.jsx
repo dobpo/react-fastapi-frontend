0c7e2513c4748eaeb372e507c894db7a7d68ea81
a.rzaev <a.rzaev@aerodisk.ru>
2023-04-11 21:58:17 +0300

    added table

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,176 +1,146 @@
 import * as React from 'react';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
+import { DataGrid } from '@mui/x-data-grid';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 
+import styles from './Table.module.scss';
+
 const columns = [
-  {id: 'name', label: 'Name', minWidth: 170},
-  {id: 'code', label: 'ISO\u00a0Code', minWidth: 100},
   {
-    id: 'population',
-    label: 'Population',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
+    field: 'first',
+    headerName: 'First',
+    minWidth: 140,
+    flex: 1,
+    headerClassName: styles.test
   },
   {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toFixed(2),
+    field: 'last',
+    headerName: 'Last',
+    minWidth: 140,
+    flex: 1,
+    headerClassName: styles.test
   },
 ];
 
-function createData(name, code, population, size) {
-  const density = population / size;
-  return {name, code, population, size, density};
-}
-
-const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
+const initialRows = [
+  {
+    id: 1,
+    first: 'Jane',
+    last: 'Carter',
+  },
+  {
+    id: 2,
+    first: 'Jack',
+    last: 'Smith',
+  },
+  {
+    id: 3,
+    first: 'Gill',
+    last: 'Martin',
+  },
 ];
 
-export default function StickyHeadTable() {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-  const [menu, setMenu] = React.useState({
-    mouseX: null,
-    mouseY: null,
-    openMenu: false
-  });
-  const [contextRow, setContextRow] = React.useState(null);
+export default function RowContextMenu() {
+  const [rows, setRows] = React.useState(initialRows);
+  const [selectedRow, setSelectedRow] = React.useState();
+
+  const [contextMenu, setContextMenu] = React.useState(null);
+
+  const handleContextMenu = (event) => {
+    event.preventDefault();
+    setSelectedRow(Number(event.currentTarget.getAttribute('data-id')));
+    setContextMenu(
+      contextMenu === null
+        ? { mouseX: event.clientX - 2, mouseY: event.clientY - 4 }
+        : null,
+    );
+  };
 
   const handleClose = () => {
-    console.log("close called");
-    setMenu({
-      openMenu: false,
-      mouseX: null,
-      mouseY: null
+    setContextMenu(null);
+  };
+
+  const convertToUppercase = () => {
+    const newRows = rows.map((row) => {
+      if (row.id === selectedRow) {
+        console.log(row.id, selectedRow)
+        return {
+          ...row,
+          first: row.first.toUpperCase(),
+          last: row.last.toUpperCase(),
+        };
+      }
+      return row;
     });
-    setContextRow(null);
+    setRows(newRows);
+    handleClose();
   };
 
-
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
+  const convertToLowercase = () => {
+    const newRows = rows.map((row) => {
+      if (row.id === selectedRow) {
+        return {
+          ...row,
+          first: row.first.toLowerCase(),
+          last: row.last.toLowerCase(),
+        };
+      }
+      return row;
+    });
+    setRows(newRows);
+    handleClose();
   };
 
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
+  const deleteRow = () => {
+    const newRows = [...rows].filter((el) => el.id !== selectedRow);
+    setRows(newRows);
+    handleClose();
   };
 
   return (
-    <Paper sx={{width: '100%', overflow: 'hidden'}}>
-      <TableContainer sx={{maxHeight: 440}}>
-        <div
-          onContextMenu={(e) => {
-            e.preventDefault();
-            // e.persist()
-            // let event = e.persist();
-            console.log("outerRightClick", contextRow, e.target.className);
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        columns={columns}
+        rows={rows}
+        // sx={{
+        //   "& .MuiDataGrid-cell": {
+        //     border: "1px solid lightgray",
+        //     // borderRadius: "5px",
+        //     backgroundColor: "white",
+        //     // width: "calc(100% - 2px)",
+        //     // marginTop: 3
+        //     },
+        // }}
+        slotProps={{
+          row: {
+            onContextMenu: handleContextMenu,
+            style: { cursor: 'context-menu' },
+          },
 
-            if (contextRow != null) {
-              setMenu({
-                openMenu: true,
-                mouseX: e.clientX - 2,
-                mouseY: e.clientY - 4
-              });
-            }
-          }}
-        >
-          <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-              <TableRow>
-                {columns.map((column) => (
-                  <TableCell
-                    key={column.id}
-                    align={column.align}
-                    style={{minWidth: column.minWidth}}
-                  >
-                    {column.label}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row) => {
-                  return (
-                    <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                      {columns.map((column) => {
-                        const value = row[column.id];
-                        return (
-                          <TableCell key={column.id} align={column.align}>
-                            {column.format && typeof value === 'number'
-                              ? column.format(value)
-                              : value}
-                          </TableCell>
-                        );
-                      })}
-                    </TableRow>
-                  );
-                })}
-            </TableBody>
-          </Table>
-          <Menu
-            keepMounted
-            open={menu.openMenu}
-            onClose={handleClose}
-            anchorReference="anchorPosition"
-            anchorPosition={
-              menu.mouseY !== null && menu.mouseX !== null
-                ? {top: menu.mouseY, left: menu.mouseX}
-                : undefined
-            }
-          >
-            <MenuItem onClick={handleClose}>Copy</MenuItem>
-            <MenuItem onClick={handleClose}>Print</MenuItem>
-            <MenuItem onClick={handleClose}>Highlight</MenuItem>
-            <MenuItem onClick={handleClose}>Email</MenuItem>
-          </Menu>
-        </div>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
+        }}
       />
-    </Paper>
+      <Menu
+        open={contextMenu !== null}
+        onClose={handleClose}
+        anchorReference="anchorPosition"
+        anchorPosition={
+          contextMenu !== null
+            ? { top: contextMenu.mouseY, left: contextMenu.mouseX }
+            : undefined
+        }
+        slotProps={{
+          root: {
+            onContextMenu: (e) => {
+              e.preventDefault();
+              handleClose();
+            },
+          },
+        }}
+      >
+        <MenuItem onClick={convertToUppercase}>UPPERCASE</MenuItem>
+        <MenuItem onClick={convertToLowercase}>lowercase</MenuItem>
+        <MenuItem onClick={deleteRow}>delete</MenuItem>
+      </Menu>
+    </div>
   );
 }
